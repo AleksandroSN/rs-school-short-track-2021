6cde6707ad55bb76fa48a3188b91a62fc33244a9
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-04-09 19:28:02 +0300

    feat: add binary search in 10-task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,32 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+// function findIndex(array, value) {
+//   // throw new Error('Not implemented');
+//   let start = 0;
+//   // так как бинарный поиск работает только на сорт массивах, можно сдедать end - 1
+//   let end = array.length - 1;
+//   let center = Math.floor((start + end) / 2);
+
+//   for (let i = 0; i < array.length; i++) {
+//     center = Math.floor((start + end) / 2);
+//     if (array[center] === value) return center;
+//     if (array[center] < value) start = center + 1;
+//     if (array[center] > value) end = center - 1;
+//   }
+
+//   return 'Upss....nothing.Your index is -1';
+// }
+function findIndex(array, value) {
+  const calcCenter = (start, end) => Math.floor((start + end) / 2);
+  const binarySearch = (arr, val, start, end) => {
+    const center = calcCenter(start, end);
+    if (end - 1 === start) return val === arr[end] ? end : start;
+    if (val > arr[center]) return binarySearch(arr, val, center, end);
+    if (val < arr[center]) return binarySearch(arr, val, start, center);
+    return center;
+  };
+  return binarySearch(array, value, 0, array.length - 1);
 }
 
 module.exports = findIndex;
